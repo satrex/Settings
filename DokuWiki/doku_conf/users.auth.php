ab820f5ec7e201053d0f181ddc0ee718b9d680fa
satrex <sat1501@gmail.com>
2013-09-30 10:45:15 +0000

    DokuWikiの設定ファイルを追加

---
+++ DokuWiki/doku_conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+satrex:eb99f1c8cedf855620cc63b4b163b52b:Satoshi Suzuki:sat1501@gmail.com:admin,user
+s-suzuki:$1$JvrJXGtw$HVATpEa0R0DmMQW0F8cNJ/:鈴木 聡:s-suzuki@fukuda-web.jp:admin,user
+s-kaori:$1$tLjAHHpR$zcKP2SRcHxzK87/BsEtEk0:鈴木 香織:s-kaori@fukuda-web.jp:user
